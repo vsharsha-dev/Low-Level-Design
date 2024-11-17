1f1c64a456850a028c824e3016770d6e2b008158
saiharshav <vsharsha48@gmail.com>
2024-11-17 22:38:32 +0530

    Add snake and ladder initial files

---
+++ SnakeAndLadder/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+  public class Player {
+
+  }
+  public class Board {
+
+  }
+  public class Dice {
+
+  }
+  public class Cell {
+
+  }
+  // Snake and Ladder can be considered as a common class because data objects are same (just start and end cells)
+  public class Impact {
+
+  }
+  public class Game {
+
+  }
+  
+  public static void main() {
+
+  }
+}
