abc48e36c6c24c95f235f03727b801f76caa8eff
saiharshav <vsharsha48@gmail.com>
2024-11-19 21:52:06 +0530

    modify snake and ladder

--- SnakeAndLadder/Main.java
+++ SnakeAndLadder/Main.java
@@ -1,11 +1,16 @@
-public class Player {
+import java.util.LinkedList;
+import java.util.Deque;
+
+class Player {
   String name;
+  int cellNum;
   Player(String name){
     this.name = name;
+    this.cellNum = 0;
   }
 }
 
-public class Cell {
+class Cell {
   int cellNumber;
   Impact snakeOrLadder;
   Cell(int num){
@@ -13,8 +18,8 @@ public class Cell {
   }
 }
 
-public class Board {
-  Cell board[10][10];
+class Board {
+  Cell[][] board = new Cell[10][10];
   void intitializeBoard(){
     for(int i=0;i<10;i++){
       for(int j=0;j<10;j++){
@@ -27,8 +32,29 @@ public class Board {
       }
     }
   }
+
+  void createImpacts() {
+    int impacts = 6;
+    while(impacts > 0){
+      int impactStart = (int)Math.random()*100;
+      int impactEnd = (int)Math.random()*100;
+      int x=0;
+      int y=0;
+      if((impactStart/10)%2 == 0){
+        x = impactStart/10;
+        y = impactStart%10;
+      }
+      else {
+        x = impactStart/10;
+        y = 10-impactStart%10;
+      }
+      board[x][y].snakeOrLadder = new Impact(impactStart, impactEnd);
+      
+      impacts--;
+    }
+  }
 }
-public class Dice {
+class Dice {
   String name;
   int roll(){
     return (int)(Math.random()*6);
@@ -36,7 +62,7 @@ public class Dice {
 }
 
 // Snake and Ladder can be considered as a Impact common class because data objects are same (just start and end cells)
-public class Impact {
+class Impact {
   int start;
   int end;
   Impact(int start, int end){
@@ -45,26 +71,54 @@ public class Impact {
   }
 }
 
-public class GameController {
-  Deque<Player> q;
+class GameController {
+  Deque<Player> q = new LinkedList<>();
   Board board;
   void play(Player p1, Player p2, Board board){
-    this.board = board
+    this.board = board;
     q.addLast(p1);
     q.addLast(p2);
     
     Dice d1 = new Dice();
-    while(1){
-      if(gameOver()){
-        return ;
+    int count = 0;
+    while(true){
+      Player currPlayer = q.removeFirst();
+      q.addLast(currPlayer);
+      int diceNum = d1.roll();
+      int currPosition = currPlayer.cellNum + diceNum;
+      if(currPosition >= 100){
+        count++;
+        System.out.println(currPlayer.name + " is No. "+Integer.toString(count)+"!");
+        q.removeLast();
+        return;
+      }
+      int x = 0;
+      int y = 0;
+      if((currPosition/10)%2 == 0){
+        x = currPosition/10;
+        y = currPosition%10;
+      }
+      else {
+        x = currPosition/10;
+        y = 10-currPosition%10;
+      }
+      if(this.board.board[x][y].snakeOrLadder != null){
+        currPlayer.cellNum = this.board.board[x][y].snakeOrLadder.end;
       }
     }
   }
+
+  
 }
 
-public class Game {
+public class Main {
   public static void main(String[] args) {
     GameController game = new GameController();
-    game.play()
+    Player p1 = new Player("P1");
+    Player p2 = new Player("P2");
+    Board b1 = new Board();
+    b1.intitializeBoard();
+    b1.createImpacts();
+    game.play(p1, p2, b1);
   }
 }
