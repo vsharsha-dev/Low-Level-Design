4fb003039131b70bb52713e4790fd71f13499a47
saiharshav <vsharsha48@gmail.com>
2024-11-19 20:16:09 +0530

    Populate classes

--- SnakeAndLadder/Main.java
+++ SnakeAndLadder/Main.java
@@ -1,25 +1,70 @@
-public class Main {
-  public class Player {
-
+public class Player {
+  String name;
+  Player(String name){
+    this.name = name;
   }
-  public class Board {
+}
 
+public class Cell {
+  int cellNumber;
+  Impact snakeOrLadder;
+  Cell(int num){
+    this.cellNumber = num;
   }
-  public class Dice {
+}
 
+public class Board {
+  Cell board[10][10];
+  void intitializeBoard(){
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+        if(i%2 == 0){
+          board[i][j] = new Cell(10*i+(j+1));
+        }
+        else {
+          board[i][j] = new Cell(10*i+(10-j));
+        }
+      }
+    }
   }
-  public class Cell {
-
+}
+public class Dice {
+  String name;
+  int roll(){
+    return (int)(Math.random()*6);
   }
-  // Snake and Ladder can be considered as a common class because data objects are same (just start and end cells)
-  public class Impact {
+}
 
+// Snake and Ladder can be considered as a Impact common class because data objects are same (just start and end cells)
+public class Impact {
+  int start;
+  int end;
+  Impact(int start, int end){
+    this.start = start;
+    this.end = end;
   }
-  public class Game {
+}
 
+public class GameController {
+  Deque<Player> q;
+  Board board;
+  void play(Player p1, Player p2, Board board){
+    this.board = board
+    q.addLast(p1);
+    q.addLast(p2);
+    
+    Dice d1 = new Dice();
+    while(1){
+      if(gameOver()){
+        return ;
+      }
+    }
   }
-  
-  public static void main() {
+}
 
+public class Game {
+  public static void main(String[] args) {
+    GameController game = new GameController();
+    game.play()
   }
 }
